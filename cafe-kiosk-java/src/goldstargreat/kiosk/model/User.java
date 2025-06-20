97bb9692dda9b934a9a95dcc50177fc29beb07e4
luckygoldstar <goldstargreat@gmail.com>
2025-06-20 21:42:30 +0900

    user

--- cafe-kiosk-java/src/goldstargreat/kiosk/model/User.java
+++ cafe-kiosk-java/src/goldstargreat/kiosk/model/User.java
@@ -1,4 +1,13 @@
 package goldstargreat.kiosk.model;
 
 public class User {
+    private final boolean isMember;
+
+    public User(boolean isMember) {
+        this.isMember = isMember;
+    }
+
+    public boolean isMember() {
+        return isMember;
+    }
 }
